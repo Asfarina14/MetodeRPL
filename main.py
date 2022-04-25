0133c0eb7eadf0de7ab9ab5c6cbfc541a5815b24
MRT Rauf <mrt.rauf@gmail.com>
2022-04-25 21:55:28 +0700

    tambah segitiga

--- main.py
+++ main.py
@@ -23,9 +23,17 @@ def hitungPersegiPanjang(p, l):
     print("Luas dari persegi panjang dengan panjang "+str(p)+" cm dan lebar "+str(l)+" cm adalah " + str(luas) + " cm")
     print("Keliling dari persegi panjang dengan panjang "+str(p)+" cm dan lebar "+str(l)+" cm adalah " + str(keliling) + " cm")
 
+def hitungSegitiga (s1, s2, s3):
+    k = (s1+s2+s3)/2 #keliling segitiga
+    l =  (k*(k-s1)*(k-s2)*(k-s3)) ** 0.5
+
+    print("Keliling Segitiga dengan sisi pertama "+str(s1)+" cm, sisi kedua "+str(s2)+", dan sisi ketiga " + str(s3) + " cm, adalah" + str(k))
+    print("Luas Segitiga dengan sisi pertama "+str(s1)+" cm, sisi kedua "+str(s2)+", dan sisi ketiga " + str(s3) + " cm, adalah" + str(l))
+
+
 print("Program untuk menghitung luas dan keliling dari lingkaran, persegi, dan persegi panjang")
-print("Pilihan kode jenis bangun datar: 1. Lingkaran   2. Persegi   3. Persegi Panjang")
-jenis = input("Masukkan kode jenis bangun datar yang diinginkan (angka 1-3): ")
+print("Pilihan kode jenis bangun datar: 1. Lingkaran   2. Persegi   3. Persegi Panjang 4. Segitiga")
+jenis = input("Masukkan kode jenis bangun datar yang diinginkan (angka 1-4): ")
 
 if jenis=="1":
     r = int(input("Masukkan ukuran panjang jari-jari lingkaran (dalam cm): "))
@@ -37,5 +45,10 @@ elif jenis=="3":
     p = int(input("Masukkan ukuran panjang dari persegi panjang (dalam cm): "))
     l = int(input("Masukkan ukuran lebar dari persegi panjang (dalam cm): "))
     hitungPersegiPanjang(p, l)
+elif jenis=="4":
+    s1 = int(input("Masukkan ukuran sisi pertama segitiga (dalam cm): "))
+    s2 = int(input("Masukkan ukuran sisi pertama segitigag (dalam cm): "))
+    s3 = int(input("Masukkan ukuran sisi pertama segitiga (dalam cm): "))
+    hitungSegitiga(s1, s2, s3)
 else:
     print("Kode yang Anda masukkan salah. Selamat tinggal.")
